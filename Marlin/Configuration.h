1d3efe98d638422696a4bdb51efac71b3fbcc841
eliteSCHW31N <business@eliteschw31n.de>
2022-05-12 21:08:32 +0200

    set steps to 2133.33 and acceleration to 50

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1016,7 +1016,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 2560, 2560, 2560 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 2133.33, 2133.33, 2133.33 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
@@ -1036,7 +1036,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 1500 }
+#define DEFAULT_MAX_ACCELERATION      { 50, 50, 50 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1051,9 +1051,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          50    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  50    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   50    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
