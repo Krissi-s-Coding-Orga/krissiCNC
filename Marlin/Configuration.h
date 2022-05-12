5f4425a39e2191287b218cbbb4f19d54c4665b4f
eliteSCHW31N <business@eliteschw31n.de>
2022-05-12 20:24:59 +0200

    remove e steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1016,7 +1016,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 2560, 2560, 2560, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 2560, 2560, 2560 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
