f7736e292e9236206fb10123ffc6ee368ed005a4
eliteSCHW31N <business@eliteschw31n.de>
2022-05-17 21:17:42 +0200

    update default feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1023,7 +1023,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 8.33, 8.33, 8.33 }
+#define DEFAULT_MAX_FEEDRATE          { 4.5, 4.5, 3.0 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
