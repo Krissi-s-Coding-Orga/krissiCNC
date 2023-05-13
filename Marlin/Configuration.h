db1a727d9f26feb91d99a88daaba78fe4ddd2a4d
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2023-05-13 20:26:57 +0200

    fix: allow minus xy movement

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1497,8 +1497,8 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -200
+#define Y_MIN_POS -200
 #define Z_MIN_POS -200
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
